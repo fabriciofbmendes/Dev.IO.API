69b79beab311911a88e27e1f7428066e011843df
Fabricio Mendes <fabriciofbmendes5@gmail.com>
2023-03-14 16:40:18 -0300

    Concluindo a modelagem da API

---
+++ src/DevIO.Api/Configuration/ApiConfig.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DevIO.Api.Configuration
+{
+    public static class ApiConfig
+    {
+        public static IServiceCollection WebApiConfig(this IServiceCollection services)
+        {
+
+            services.AddControllers();
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy("Development",
+                    builder => builder.AllowAnyOrigin()
+                        .AllowAnyMethod()
+                        .AllowAnyHeader());
+            });
+
+            return services;
+        }
+
+        public static IApplicationBuilder UseWebApiConfig(this WebApplication app)
+        {
+            app.UseHttpsRedirection();
+
+            app.UseCors("Development");
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+            return app;
+        }
+    }
+}
--- src/DevIO.Api/Program.cs
+++ src/DevIO.Api/Program.cs
@@ -1,51 +1,32 @@
 using DevIO.Api.Configuration;
 using DevIO.Data.Context;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.AspNetCore.Cors;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddAutoMapper(typeof(Program));
-builder.Services.ResolveDependencies();
 builder.Services.AddDbContext<MeuDbContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
-builder.Services.Configure<ApiBehaviorOptions>(options =>
-{
-    options.SuppressModelStateInvalidFilter = true;
-});
 
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("Development", 
-        builder => builder
-    .AllowAnyMethod()
-    .AllowAnyHeader()
-    .SetIsOriginAllowed(origin => true)
-    .AllowCredentials());
-});
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+builder.Services.WebApiConfig();
+
+builder.Services.ResolveDependencies();
+
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseDeveloperExceptionPage();
+}
+else
+{
+    app.UseHsts();
 }
 
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-app.UseCors("Development");
-app.MapControllers();
+app.UseWebApiConfig();
 
 app.Run();
